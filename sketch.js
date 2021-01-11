b9ec36afa67f8b0a8a6653af91034026ed93d266
Ssamarjeet6570 <ssamarjeet6570@gmail.com>
2021-01-11 17:04:10 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -3,22 +3,25 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world;
+var engine, world,score;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
+
 
 var gameState = "onSling";
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
+
+    getTime();
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
+    
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
@@ -39,6 +42,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(200,50);
+    score=0;
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
@@ -47,26 +51,59 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+
+textSize(35);
+fill ("white");
+    text("score"+score,600,50);
+    
+
     //strokeWeight(4);
     box1.display();
+
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
+    
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
     log4.display();
     log5.display();
 
+    
+
     bird.display();
     platform.display();
     //log6.display();
     slingshot.display();    
+}
+
+async function getTime(){
+//var response = await fetch("http://worldtimeapi.org/api/timezone/America/New_York");
+var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+var responseJson=await response.json();
+console.log(responseJson);
+var datetime=responseJson.datetime;
+var hour=datetime.slice(11,13);
+console.log(hour);
+var bg;
+if(hour>=6 && hour <= 18){
+    bg = "sprites/bg.png";
+
+}
+else{
+    bg = "sprites/bg2.jpg";
+}
+
+console.log(bg);
+backgroundImg = loadImage(bg);
 }
 
 function mouseDragged(){
@@ -86,3 +123,5 @@ function keyPressed(){
        // slingshot.attach(bird.body);
     }
 }
+
+
